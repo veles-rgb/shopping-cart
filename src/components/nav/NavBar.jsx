3bc26888ed15b45cf1fdc5fa421b9891669c0c42
MacMini <181009546+veles-rgb@users.noreply.github.com>
2025-09-04 16:40:31 -0400

    Move links into a UL with LI's

--- src/components/nav/NavBar.jsx
+++ src/components/nav/NavBar.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import { NavLink } from 'react-router';
+import '../nav/NavBar.module.css';
 
 const NavBar = () => {
   return (
     <nav>
       <h1>VelesShop</h1>
       <input type="search" name="" id="" />
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/shop">Shop</NavLink>
-      <NavLink to="/cart">Cart</NavLink>
+      <ul>
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/shop">Shop</NavLink>
+        </li>
+        <li>
+          <NavLink to="/cart">Cart</NavLink>
+        </li>
+      </ul>
     </nav>
   );
 };
