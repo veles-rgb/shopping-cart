bb233e92c463a439cade6fe74177f7864011584c
MacMini <181009546+veles-rgb@users.noreply.github.com>
2025-09-05 15:22:16 -0400

    Create structure for home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import { useNavigate } from 'react-router';
+import styles from './Home.module.css';
+import hero from '/src/assets/hero.jpg';
 
-const Home = () => {
-  return <div>Home</div>;
-};
+export default function Home() {
+  const navigate = useNavigate();
 
-export default Home;
+  const handleShopNowClick = () => {
+    navigate('/shop');
+  };
+
+  return (
+    <div className={styles.page} style={{ backgroundImage: `url(${hero})` }}>
+      <div className={styles.overlay} />
+      <div className={styles.content}>
+        <h1>Welcome to VelesShop</h1>
+        <p>
+          Welcome to VelesShop — where browsing is simple, and checkout is a
+          breeze. Start exploring our collection and find something you’ll love
+          today.
+        </p>
+        <button onClick={handleShopNowClick}>SHOP NOW</button>
+      </div>
+    </div>
+  );
+}
