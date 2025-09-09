320c0fb2198f632ecd86a90d66933bb73d93ec4b
MacMini <181009546+veles-rgb@users.noreply.github.com>
2025-09-09 16:17:46 -0400

    Create cart page using useOutletContext

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import styles from './Cart.module.css';
+import { useOutletContext } from 'react-router-dom';
 
-const Cart = () => {
-  return <div>Cart</div>;
-};
+export default function Cart() {
+  const { cart, removeFromCart, updateQty, clearCart } = useOutletContext();
+  if (cart.length === 0)
+    return <div className={styles.empty}>Cart is empty.</div>;
+  return (
+    <div>
+      <button onClick={() => clearCart()}>Clear Cart</button>
+      {cart.map((item) => (
+        <div key={item.id}>
+          <img src={item.image} alt="" />
+          <h3>{item.title}</h3>
+          <div>
+            <button onClick={() => updateQty(item.id, item.qty - 1)}>-</button>
+            <input
+              type="number"
+              min="1"
+              value={item.qty}
+              onChange={(e) => updateQty(item.id, Number(e.target.value))}
+            />
+            <button onClick={() => updateQty(item.id, item.qty + 1)}>+</button>
+          </div>
 
-export default Cart;
+          <button onClick={() => removeFromCart(item.id)}>Remove</button>
+        </div>
+      ))}
+    </div>
+  );
+}
