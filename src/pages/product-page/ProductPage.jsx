0116830754f16292d385791c44a92b0406c29999
MacMini <181009546+veles-rgb@users.noreply.github.com>
2025-09-09 14:04:41 -0400

    Create dynamic route/product page with fetch API
    Uses useParams to get productId from URL and renders product information.

--- src/pages/product-page/ProductPage.jsx
+++ src/pages/product-page/ProductPage.jsx
@@ -1,7 +1,39 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
 
-const ProductPage = () => {
-  return <div>ProductPage</div>;
-};
+export default function ProductPage() {
+  const { productId } = useParams();
+  const [product, setProduct] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-export default ProductPage;
+  useEffect(() => {
+    fetch(`https://fakestoreapi.com/products/${productId}`)
+      .then((response) => {
+        if (response.status >= 400) {
+          throw new Error('Server error');
+        }
+        return response.json();
+      })
+      .then((data) => setProduct(data))
+      .catch((error) => setError(error))
+      .finally(() => setLoading(false));
+  }, [productId]);
+
+  if (!product) return <div>Product Not Found</div>;
+  if (loading) return <div>Loading…</div>;
+  if (error) return <div>Error: {error.message}</div>;
+
+  return (
+    <div>
+      <img src={product.image} alt="" />
+      <h1>{product.title}</h1>
+      <p>{product.description}</p>
+      <strong>${product.price}</strong>
+      <div>
+        <button>Add to Cart</button>
+        <button>Buy Now</button>
+      </div>
+    </div>
+  );
+}
