c136b12f77e633026caf9ed84aad26d03a17b93a
MacMini <181009546+veles-rgb@users.noreply.github.com>
2025-09-10 14:17:11 -0400

    Update classes for styling

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,18 +3,21 @@ import styles from './Footer.module.css';
 
 const Footer = () => {
   return (
-    <>
-      <small className={styles.small}>
-        <a
-          href="https://github.com/veles-rgb/shopping-cart"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          <FaGithub size={24} color="white" />
-          {'veles-rgb'}
-        </a>
+    <footer className={styles.footer}>
+      <a
+        href="https://github.com/veles-rgb/shopping-cart"
+        rel="noopener noreferrer"
+        target="_blank"
+        className={styles.link}
+      >
+        <FaGithub size={20} />
+        <span>veles-rgb</span>
+      </a>
+      <small className={styles.copy}>
+        © {new Date().getFullYear()} VelesShop. All rights reserved.
       </small>
-    </>
+    </footer>
   );
 };
+
 export default Footer;
